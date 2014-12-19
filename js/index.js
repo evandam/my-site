2593e2d07602be6c8c92435f0cd158ee317b88e3
evandam <evandam92@gmail.com>
2014-12-18 21:49:35 -0500

    playing with colors

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+	$(window).resize(resize);
+	resize();
+
 	// generate lorem ipsum dummy text
 	$('.ipsum').each(function() {
 		lorem_ipsum(this, $(this).attr('ipsum-paras'));
 	});
+
+	// autoRefresh(5000);
 });
+
+function resize() {
+	$('main > div').css('min-height',
+		$(window).height() - $('.navbar').height()
+	);
+}
+
+function autoRefresh(interval) {
+	setTimeout(function() {
+		location.reload();
+	}, interval);
+}
 
 function lorem_ipsum(el, paras) {
 	var url = 'http://baconipsum.com/api/?type=meat-and-filler';
