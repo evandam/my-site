ddc58178ea27ead73045477a94fa69e9b2e2cd01
Evan Van Dam <evandam92@gmail.com>
2015-01-04 15:27:21 +0000

    start angular branch - no django

--- myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Section(models.Model):
-	def __unicode__(self):
-		return self.title
-
-	title = models.CharField(max_length=50)
-	htmlFile = models.CharField(max_length=100)
--- myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from myapp.models import Section
-
-# Create your views here.
-def index(request):
-	return render(request, 'myapp/index.html', {'sections': Section.objects.all()})
