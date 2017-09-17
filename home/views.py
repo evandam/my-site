0a3a580b27170fb638a7f3a13aff5d2b19df30c2
Evan <evandam92@gmail.com>
2017-09-17 19:48:45 -0400

    added an intro blurb

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {'title': 'Evan Van Dam'}
     return render(request, 'home/index.html', context)
